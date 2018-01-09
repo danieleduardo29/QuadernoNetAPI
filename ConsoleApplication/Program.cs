d39b398fbf1a90770d87bd7cd9e41fac283241b8
danieleduardo29 <danieleduardo29@gmail.com>
2018-01-09 13:19:41 -0500

    Initial commit
    Connection (ping only) to a Quaderno sandbox account

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QuadernoNetAPI;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string privateKey = ConfigurationManager.AppSettings["QuadernoPrivateKey"];
+            string publicKey = ConfigurationManager.AppSettings["QuadernoPublicKey"];
+            string apiURL = ConfigurationManager.AppSettings["QuadernoURL"];
+
+            QuadernoBase.Init(privateKey, apiURL);
+            QuadernoBase.Ping();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuadernoNetAPI/QuadernoBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace QuadernoNetAPI
+{
+    public class QuadernoBase
+    {
+        private static string API_KEY = null;
+        private static string URL = null;
+
+        public static void Init(string apiKey, string url)
+        {
+            API_KEY = apiKey;
+            URL = url;
+        }
+
+        public static bool Ping()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://sandbox-quadernoapp.com/api/");
+            var byteArray = new UTF8Encoding().GetBytes(API_KEY);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            
+            HttpResponseMessage response = client.GetAsync("ping.json").Result;
+            HttpContent content = response.Content;
+
+            // ... Check Status Code                                
+            Console.WriteLine("Response StatusCode: " + (int)response.StatusCode);
+
+            // ... Read the string.
+            string result = content.ReadAsStringAsync().Result;
+
+            // ... Display the result.
+            Console.WriteLine(result);
+            
+            return true;
+        }
+    }
+}
